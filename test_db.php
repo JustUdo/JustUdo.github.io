686cd0a4ededdb9ef1637261c5074b3596fc7812
JustUdo <ueokeke@aggies.ncat.edu>
2025-03-31 16:30:30 -0400

    Delete test_db.php

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('mysqli_connect.php');
-
-if ($dbc) {
-    echo "Database connected successfully!";
-} else {
-    echo "Database connection failed: " . mysqli_connect_error();
-}
-?>
